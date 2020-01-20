70e3232ef0698889f44e9238449804ab6f4cf178
Heri Setiawan <heri.setiawan@dkatalis.com>
2020-01-20 16:57:32 +0700

    Adding some test

---
+++ test/basic.test.js
@@ -0,0 +1,25 @@
+const Lab = require('@hapi/lab')
+const { expect } = require('@hapi/code')
+const { it, beforeEach, afterEach, describe } = exports.lab = Lab.script()
+const { init } = require('../lib/server')
+
+describe('GET /', () => {
+  let server
+
+  beforeEach(async () => {
+    server = await init()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  it('Try open API', async () => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/'
+    })
+    expect(res.result.message).to.equal('Hello World')
+  })
+}
+)
