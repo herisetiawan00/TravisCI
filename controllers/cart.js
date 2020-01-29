17d7c118e1cf6caff40730104d7f431837651ed1
Heri Setiawan <heri.setiawan@dkatalis.com>
2020-01-29 14:00:38 +0700

    fixing add cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,38 +1,52 @@
 const { Cart } = require('../models/cartModel')
 const { User } = require('../models/userModel')
 
+const checkEmail = async (email, password) => {
+  const getEmail = await User.findOne({ email: email })
+  return getEmail != null && getEmail.password === password
+}
+
+const cartSaver = async ({ exist, cart, product, email, products }) => {
+  if (!exist) {
+    cart.products.push(product)
+    await cart.save()
+  } else {
+    await Cart.updateOne({ user: email }, { products: cart.products })
+  }
+}
+
+const addCart = async ({ email, id }) => {
+  const cart = await Cart.findOne({ user: email })
+  const product = { id: id, quantity: 1 }
+  if (cart == null) {
+    const cart = { user: email, products: [] }
+    cart.products.push(product)
+    await Cart.insertMany(cart)
+  } else {
+    let found = false
+    for (var i = 0; i < cart.products.length; i++) {
+      if (cart.products[i].id === id) {
+        found = true
+        cart.products[i].quantity = cart.products[i].quantity + 1
+      }
+    }
+    cartSaver({
+      exist: found,
+      cart: cart,
+      product: product,
+      email: email
+    })
+  }
+}
 exports.getCart = async (req, h) => {
-  const getEmail = await User.findOne({ email: req.payload.email })
-  if (getEmail != null && getEmail.password === req.payload.password) {
+  if (checkEmail(req.payload.email, req.payload.password)) {
     return Cart.find({ user: req.payload.email })
   }
 }
 
 exports.addCart = async (req, h) => {
-  const getEmail = await User.findOne({ email: req.payload.email })
-  if (getEmail != null && getEmail.password === req.payload.password) {
-    const cart = await Cart.findOne({ user: req.payload.email })
-    const product = { id: req.payload.id, quantity: 1 }
-    if (cart == null) {
-      const cart = { user: req.payload.email, products: [] }
-      cart.products.push(product)
-      await Cart.insertMany(cart)
-    } else {
-      let found = false
-      for (var i = 0; i < cart.products.length; i++) {
-        if (cart.products[i].id === req.payload.id) {
-          found = true
-          cart.products[i].quantity = cart.products[i].quantity + 1
-        }
-      }
-      console.log(found)
-      if (!found) {
-        cart.products.push(product)
-        await cart.save()
-      } else {
-        await Cart.updateOne({ user: req.payload.email }, { products: cart.products })
-      }
-    }
+  if (checkEmail(req.payload.email, req.payload.password)) {
+    addCart({ email: req.payload.email, id: req.payload.id })
     return h.response({ success: true }).code(201)
   } else {
     return h.response({ success: false, message: 'Forbidden' }).code(403)
